5128ed956be9157a8bc25785e7e532b1ff589f6f
Gabriel Parada <106406575+gabrielparadaf@users.noreply.github.com>
2022-11-24 10:35:45 -0600

    test 2

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/erc20/ERC20Wallet.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/ethereum/erc20/ERC20Wallet.java
@@ -24,8 +24,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthEstimateGas;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
